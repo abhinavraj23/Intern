0b9f8317a5bf884c2f3405a4a14c7abaf80aa27a
harsh-2711 <harshaudi27@gmail.com>
2018-06-30 19:12:56 +0530

    Added bottom navigation functionality and modified welcome message

---
+++ app/src/main/java/com/daiict/developers/intern/BottomBarAdapter.java
@@ -0,0 +1,29 @@
+package com.daiict.developers.intern;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BottomBarAdapter extends SmartFragmentStatePagerAdapter {
+    private final List<Fragment> fragments = new ArrayList<>();
+
+    public BottomBarAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+
+    public void addFragments(Fragment fragment) {
+        fragments.add(fragment);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+}
